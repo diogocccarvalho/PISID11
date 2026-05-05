1bd739048a082891821554acf805cb9afbb5399f
Tomas Salvador <tomas.f.l.salvador@gmail.com>
2026-05-05 17:26:36 +0100

    Logica 2 pcs feita

--- database/cloud_mysql_connect.py
+++ database/cloud_mysql_connect.py
@@ -1,8 +1,19 @@
 import mysql.connector
+import os
+import configparser
+
+_config = configparser.ConfigParser()
+_config.read(os.path.join(os.path.dirname(__file__), '..', 'config.ini'))
+
+_host = _config.get('cloud_mysql', 'host', fallback ='194.210.86.10')
+_user = _config.get('cloud_mysql', 'user', fallback  ='aluno')
+_password = _config.get('cloud_mysql', 'password', fallback = 'aluno')
+_database = _config.get('cloud_mysql', 'database', fallback ='maze')
+
 
 db_config = {
-    "host": "194.210.86.10",
-    "user": "aluno",
-    "password": "aluno",
-    "database": "maze"
+    "host": _host,
+    "user": _user,
+    "password": _password,
+    "database": _database
 }
--- database/mongo_connect.py
+++ database/mongo_connect.py
@@ -1,4 +1,13 @@
+import configparser
+import os
 from pymongo import MongoClient
 
-client_mongo = MongoClient("mongodb://localhost:27017/")
-db = client_mongo["pisid"]
+_config = configparser.ConfigParser()
+_config.read(os.path.join(os.path.dirname(__file__), '..', 'config.ini'))
+
+_host = _config.get('mongo', 'host', fallback='localhost')
+_port = _config.getint('mongo', 'port', fallback=27017)
+_database = _config.get('mongo', 'database', fallback='pisid')
+
+client_mongo = MongoClient(f"mongodb://{_host}:{_port}/")
+db = client_mongo[_database]
